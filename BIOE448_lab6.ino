698e4845699a272573fd27409090ed7bcd91fed9
DaniNajmi <dnajmiasl@gmail.com>
2025-02-26 13:42:04 -0600

    Adding storage

--- BIOE448_lab6.ino
+++ BIOE448_lab6.ino
@@ -3,6 +3,13 @@ int pulse_signal = 0;
 float BPM = 0;
 
 
+// Declaring storage data
+bool any_peak_detected = false;
+bool first_peak_detected = false;
+unsigned long first_pulse_time = 0;
+unsigned long second_pulse_time = 0;
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -12,6 +19,13 @@ void setup() {
 void loop() {
   pulse_signal = analogRead(sensor_pin);
   Serial.println(pulse_signal);
+
+  delay(75);
+
+
+
+
+
 }
 
 
